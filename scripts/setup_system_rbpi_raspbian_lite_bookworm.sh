c5680537e9de1c1e59151c78826e49458848edac
Anupam Basak <anupam.basak27@gmail.com>
2023-07-03 12:10:58 +0530

    Fix missing package for bookworm

--- scripts/setup_system_rbpi_raspbian_lite_bookworm.sh
+++ scripts/setup_system_rbpi_raspbian_lite_bookworm.sh
@@ -114,7 +114,7 @@ CLI_TOOLS_PACKAGES="raspi-config psmisc tree joe nano vim p7zip-full i2c-tools \
 	fbi scrot mpg123  mplayer xloadimage imagemagick fbcat abcmidi evtest libts-bin"
 
 PYTHON_PACKAGES="python3 python3-dev cython3 python3-cffi python3-tk python3-dbus python3-mpmath python3-pil python3-pip \
-	python3-pil.imagetk python3-setuptools python3-pyqt5 python3-numpy-dev python3-evdev 2to3 python-is-python2 "
+	python3-pil.imagetk python3-setuptools python3-pyqt5 python3-numpy-dev python3-evdev 2to3 python-is-python3"
 
 apt-get -y install $SYSTEM_PACKAGES $CLI_TOOLS_PACKAGES $PYTHON_PACKAGES
 
