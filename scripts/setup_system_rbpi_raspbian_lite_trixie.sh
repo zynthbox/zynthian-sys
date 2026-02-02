cf588001a1619ee5034e13efd545ad022655b822
Anupam Basak <anupam.basak27@gmail.com>
2026-02-02 12:09:01 +0530

    Update system package list for building zynthbox trixie image

--- scripts/setup_system_rbpi_raspbian_lite_trixie.sh
+++ scripts/setup_system_rbpi_raspbian_lite_trixie.sh
@@ -83,7 +83,7 @@ CLI_TOOLS_PACKAGES="raspi-config psmisc tree joe nano vim p7zip-full i2c-tools \
 fbi scrot xloadimage imagemagick fbcat abcmidi evtest libts-bin gpiod libgpiod-dev openmpt123"
 
 PYTHON_PACKAGES="python3 python3-dev cython3 python3-cffi python3-dbus python3-mpmath python3-pil python3-pip \
-python3-setuptools python3-numpy-dev python3-evdev 2to3 python-is-python3 python3-tk python3-pil.imagetk"
+python3-setuptools python3-numpy-dev python3-evdev python-is-python3"
 
 apt-get -y -o Dpkg::Options::="--force-confdef" install $SYSTEM_PACKAGES $CLI_TOOLS_PACKAGES $PYTHON_PACKAGES
 
@@ -99,14 +99,14 @@ qt5-qmake gobjc++ ruby rake xsltproc vorbis-tools zenity"
 AV_LIBS_PACKAGES="libavformat-dev libavcodec-dev ffmpeg"
 # Libraries
 LIBS_PACKAGES="libfftw3-dev libmxml-dev zlib1g-dev fluid libfltk1.3-dev libfltk1.3-compat-headers \
-libncurses5-dev liblo-dev dssi-dev libjpeg-dev libxpm-dev libcairo2-dev libglu1-mesa-dev \
+libncurses-dev liblo-dev dssi-dev libjpeg-dev libxpm-dev libcairo2-dev libglu1-mesa-dev \
 libasound2-dev dbus-x11 a2jmidid libffi-dev fontconfig-config \
-libfontconfig1-dev libxft-dev libexpat-dev libglib2.0-dev libgettextpo-dev libsqlite3-dev \
-libglibmm-2.4-dev libeigen3-dev libsndfile-dev libsamplerate-dev libarmadillo-dev libreadline-dev \
-lv2-c++-tools libxi-dev libgtk2.0-dev libgtkmm-2.4-dev liblrdf-dev libboost-system-dev libzita-convolver-dev \
+libfontconfig1-dev libxft-dev libexpat1-dev libglib2.0-dev libgettextpo-dev libsqlite3-dev \
+libglibmm-2.4-dev libeigen3-dev libsndfile1-dev libsamplerate0-dev libarmadillo-dev libreadline-dev \
+lv2-c++-tools libxi-dev libgtk2.0-dev libgtkmm-2.4-dev liblrdf0-dev libboost-system-dev libzita-convolver-dev \
 libzita-resampler-dev fonts-roboto libxcursor-dev libxinerama-dev mesa-common-dev libgl1-mesa-dev \
-libfreetype6-dev  libswscale-dev  qtbase5-dev qtdeclarative5-dev libcanberra-gtk-module \
-libcanberra-gtk3-module libxcb-cursor-dev libgtk-3-dev libxcb-util0-dev libxcb-keysyms1-dev libxcb-xkb-dev \
+libfreetype-dev  libswscale-dev  qtbase5-dev qtdeclarative5-dev libcanberra-gtk3-module \
+libxcb-cursor-dev libgtk-3-dev libxcb-util0-dev libxcb-keysyms1-dev libxcb-xkb-dev \
 libxkbcommon-x11-dev libssl-dev libtag1-dev"
 EXTRA_PACKAGES="jack-midi-clock midisport-firmware"
 
