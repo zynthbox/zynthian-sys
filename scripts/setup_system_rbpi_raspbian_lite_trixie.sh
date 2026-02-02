f0adf784b32f03088e8cc63dbc804c26790c77f0
probal31 <probal31@gmail.com>
2026-02-02 18:22:19 +0530

    Removed and added packages for trixie

--- scripts/setup_system_rbpi_raspbian_lite_trixie.sh
+++ scripts/setup_system_rbpi_raspbian_lite_trixie.sh
@@ -42,7 +42,9 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get -y update --allow-releaseinfo-change
 
 # Install required dependencies if needed
-apt-get -y install apt-utils apt-transport-https rpi-update sudo software-properties-common parted dirmngr rpi-eeprom gpgv ca-certificates
+# Remove the following packages for trixie as they are not needed anymore:
+# software-properties-common
+apt-get -y install apt-utils apt-transport-https rpi-update sudo  parted dirmngr rpi-eeprom gpgv ca-certificates
 
 # Update Firmware
 if [ "$ZYNTHIAN_INCLUDE_RPI_UPDATE" == "yes" ]; then
@@ -71,8 +73,10 @@ apt-get -y autoremove
 #------------------------------------------------
 
 # System
+# Removed the following packages for trixie as they are not needed anymore:
+# dhcpcd-dbus
 apt-get -y remove --purge isc-dhcp-client triggerhappy logrotate dphys-swapfile
-SYSTEM_PACKAGES="systemd avahi-daemon dhcpcd-dbus usbutils udisks2 udevil exfatprogs \
+SYSTEM_PACKAGES="systemd avahi-daemon usbutils udisks2 udevil exfatprogs \
 xinit xserver-xorg-video-fbdev x11-xserver-utils xinput libgl1-mesa-dri tigervnc-standalone-server \
 xdotool cpufrequtils wpasupplicant wireless-tools iw dnsmasq firmware-brcm80211 firmware-atheros \
 firmware-realtek atmel-firmware firmware-misc-nonfree shiki-colors-xfwm-theme fonts-freefont-ttf \
@@ -123,48 +127,57 @@ MOD_UI_PIP3_PACKAGES="pyserial pystache aggdraw pycrypto"
 pip3 install --upgrade pip
 pip3 install $PIP3_PACKAGES $ZYNTHBOX_PIP3_PACKAGES $MOD_UI_PIP3_PACKAGES
 
-ZYNTHBOX_OTHER_DEPENDENCIES="zynthbox-dependency-mod-host zynthbox-dependency-mod-browsepy zynthian-data zynthbox-dependency-mod-ui plasma-framework"
+# Removed the following packages for trixie as they are not used anymore:
+# zynthbox-dependency-mod-browsepy, zynthbox-dependency-mod-ui
+ZYNTHBOX_OTHER_DEPENDENCIES="zynthbox-dependency-mod-host zynthian-data plasma-framework"
 
 UPDATABLE_PACKAGES="$(cat $ZYNTHIAN_SYS_DIR/scripts/updatable_packages.list)"
 
 # Install ZynthboxQML and its dependencies
 apt-get -y --allow-unauthenticated install \
 	breeze-icon-theme \
-	jack-capture \
 	libtag1-dev \
 	libwebkit2gtk-4.0-37 \
 	matchbox-window-manager \
 	python3-alsaaudio \
-	python3-pyside2.qtcharts \
 	python3-pyside2.qtconcurrent \
 	python3-pyside2.qtcore \
 	python3-pyside2.qtgui \
 	python3-pyside2.qthelp \
-	python3-pyside2.qtlocation \
 	python3-pyside2.qtmultimedia \
 	python3-pyside2.qtmultimediawidgets \
 	python3-pyside2.qtnetwork \
 	python3-pyside2.qtopengl \
-	python3-pyside2.qtpositioning \
+	python3-pyside2.qtopenglfunctions \
 	python3-pyside2.qtprintsupport \
 	python3-pyside2.qtqml \
 	python3-pyside2.qtquick \
-	python3-pyside2.qtquickwidgets \
-	python3-pyside2.qtscript \
-	python3-pyside2.qtscripttools \
-	python3-pyside2.qtsensors \
 	python3-pyside2.qtsql \
 	python3-pyside2.qtsvg \
 	python3-pyside2.qttest \
-	python3-pyside2.qttexttospeech \
 	python3-pyside2.qtuitools \
 	python3-pyside2.qtwebchannel \
+	python3-pyside2.qtwebengine \
+	python3-pyside2.qtwebenginecore \
+	python3-pyside2.qtwebenginewidgets \
 	python3-pyside2.qtwebsockets \
 	python3-pyside2.qtwidgets \
-	python3-pyside2.qtwidgets \
-	python3-pyside2.qtx11extras \
 	python3-pyside2.qtxml \
 	python3-pyside2.qtxmlpatterns \
+	python3-pyside6.qt3danimation \
+	python3-pyside6.qt3dcore \
+	python3-pyside6.qt3dextras \
+	python3-pyside6.qt3dinput \
+	python3-pyside6.qt3dlogic \
+	python3-pyside6.qt3drender \
+	python3-pyside6.qtasyncio \
+	python3-pyside6.qtbluetooth \
+	python3-pyside6.qtcharts \
+	python3-pyside6.qtconcurrent \
+	python3-pyside6.qtcore \
+	python3-pyside6.qtdatavisualization \
+	python3-pyside6.qtdbus \
+	python3-pyside6.qtdesigner \
 	python3-xlib \
 	qml-module-org-kde-newstuff \
 	qml-module-qt-labs-folderlistmodel \
