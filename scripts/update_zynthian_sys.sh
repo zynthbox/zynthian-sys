a8d00267f471400263f6c94a014a084910148d50
Anupam Basak <anupam.basak27@gmail.com>
2026-02-25 12:39:39 +0530

    Wireplumber : Install wireplumber config to set default source/sink volume to 1.0

--- scripts/update_zynthian_sys.sh
+++ scripts/update_zynthian_sys.sh
@@ -403,6 +403,7 @@ if [ -z "$NO_ZYNTHIAN_UPDATE" ]; then
 	cp -a $ZYNTHIAN_SYS_DIR/etc/modprobe.d/* /etc/modprobe.d
 	cp -an $ZYNTHIAN_SYS_DIR/etc/vim/* /etc/vim
 	cp -a $ZYNTHIAN_SYS_DIR/etc/update-motd.d/* /etc/update-motd.d
+	cp -a $ZYNTHIAN_SYS_DIR/etc/wireplumber/ /etc/wireplumber
 	# WIFI Hotspot
 	cp -an $ZYNTHIAN_SYS_DIR/etc/hostapd/* /etc/hostapd
 	cp -a $ZYNTHIAN_SYS_DIR/etc/dnsmasq.conf /etc
